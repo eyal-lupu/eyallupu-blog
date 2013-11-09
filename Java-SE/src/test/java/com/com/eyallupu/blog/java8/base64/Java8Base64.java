ad547342699bf63c0e5f6d971511842e5d2f3293
Eyal Lupu <eyall2@gmail.com>
2013-11-09 21:57:37 +0000

    Java 8 Base64 Support examples (for blog entry)

--- Java-SE/src/test/java/com/com/eyallupu/blog/java8/base64/Java8Base64.java
+++ Java-SE/src/test/java/com/com/eyallupu/blog/java8/base64/Java8Base64.java
@@ -2,8 +2,8 @@ package com.com.eyallupu.blog.java8.base64;
 
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,7 +12,7 @@ import java.util.Base64;
 import java.util.UUID;
 
 /**
- * Samples of Jave 8 Base64 support.
+ * Samples of Java 8 Base64 support.
  *
  * @author Eyal Lupu
  */
@@ -29,43 +29,49 @@ public class Java8Base64 {
         System.out.println(new String(asBytes, "utf-8"));
     }
 
+
     @Test
     public void wrapping() throws IOException {
 
+        String src = "This is the content of any resource read from somewhere" +
+                " into a stream. This can be text, image, video or any other stream.";
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try (OutputStream os = Base64.getEncoder().wrap(baos)) {
-            os.write("this is an example\n".getBytes("utf-8"));
-            os.write("of stream wrapping".getBytes("utf-8"));
+        // An encoder wraps an OutputStream. The content of /tmp/buff-base64.txt will be the
+        // Base64 encoded form of src.
+        try (OutputStream os = Base64.getEncoder().wrap(new FileOutputStream("/tmp/buff-base64.txt"))) {
+            os.write(src.getBytes("utf-8"));
         }
-        byte[] bytes = baos.toByteArray();
 
-        try (InputStream is = Base64.getDecoder().wrap(new ByteArrayInputStream(bytes))) {
+        // The section bellow illustrates a wrapping of an InputStream and decoding it as the stream
+        // is being consumed. There is no need to buffer the content of the file just for decoding it.
+        try (InputStream is = Base64.getDecoder().wrap(new FileInputStream("/tmp/buff-base64.txt"))) {
             int len;
+            byte[] bytes = new byte[100];
             while ((len = is.read(bytes)) != -1) {
-                System.out.println(new String(bytes, 0, len, "utf-8"));
+                System.out.print(new String(bytes, 0, len, "utf-8"));
             }
         }
     }
 
     @Test
     public void urlEncode() throws UnsupportedEncodingException {
-        byte[] urlEncoded = Base64.getUrlEncoder().encode("subjects?abcd".getBytes("utf-8"));
-        byte[] basicEncoded = Base64.getEncoder().encode("subjects?abcd".getBytes("utf-8"));
+        String basicEncoded = Base64.getEncoder().encodeToString("subjects?abcd".getBytes("utf-8"));
+        System.out.println("Using Basic Alphabet: " + basicEncoded);
 
-        System.out.println(new String(urlEncoded, "utf-8"));
-        System.out.println(new String(basicEncoded, "utf-8"));
+        String urlEncoded = Base64.getUrlEncoder().encodeToString("subjects?abcd".getBytes("utf-8"));
+        System.out.println("Using URL Alphabet: " + urlEncoded);
     }
 
 
     @Test
     public void mimeEncode() throws UnsupportedEncodingException {
         StringBuilder sb = new StringBuilder();
-        for (int t = 0; t < 100; ++t) {
+        for (int t = 0; t < 10; ++t) {
             sb.append(UUID.randomUUID().toString());
         }
         byte[] toEncode = sb.toString().getBytes("utf-8");
-        byte[] urlEncoded = Base64.getMimeEncoder().encode(toEncode);
-        System.out.println(new String(urlEncoded, "utf-8"));
+        String mimeEncoded = Base64.getMimeEncoder().encodeToString(toEncode);
+        System.out.println(mimeEncoded);
     }
+
 }
