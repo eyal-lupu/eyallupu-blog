9b8b0a47cb4644ecf24f8143d078335c022e1411
Eyal Lupu <elupu@websense.com>
2016-04-10 14:09:06 +0100

    Initial version

---
+++ session-fixation-demo/src/main/java/com/eyallupu/blog/SessionFixationDemoApplication.java
@@ -0,0 +1,12 @@
+package com.eyallupu.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionFixationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionFixationDemoApplication.class, args);
+	}
+}
